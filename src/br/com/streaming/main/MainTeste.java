7d7159f583d5677d410c0fe4baeaeaf1ede71de1
unknown <igortrabalhos2006@gamil.com>
2026-08-06 09:31:44 -0300

    feat: implementa toString, equals, static e final no Usuario

--- src/br/com/streaming/main/MainTeste.java
+++ src/br/com/streaming/main/MainTeste.java
@@ -1,10 +1,44 @@
 package br.com.streaming.main;
-
+import br.com.streaming.model.Usuario;
 // TODO: Fazer imports das classes dos pacotes model e financeiro
 
 public class MainTeste {
     public static void main(String[] args) {
         System.out.println("=== SISTEMA DE STREAMING ===");
+        System.out.println(); // pula linha.
+        Usuario u1 = new Usuario(null, null);
+        u1.setNome("Joao Silva");
+        u1.setEmail("joao@email.com");
+        Usuario u2 = new Usuario(null, null);
+        u2.setNome("Marcos");
+        u2.setEmail("marcos1@email.com");
+        Usuario u3 = new Usuario(null, null);
+        u3.setNome("Bob");
+        u3.setEmail("Bob@email.com");
+
+        System.out.println(u1);
+        System.out.println();
+        System.out.println(u2);
+        System.out.println();
+        System.out.println(u3);
+        System.out.println();
+        // agora estavamos usando objetos agora printamos usando uma classe.
+        System.out.println(Usuario.NOME_PLATAFORMA);
+        System.out.println();
+        // contador slide 43.
+        System.out.println("Total de Contas: " + Usuario.contadorUsuarios);
+        System.out.println();
         // TODO: Testes da Aula 2 e 3
-    }
+// codigo esta comparando as casas no caso (new) usar equals compara strings ai so caso estamos comparando objetos. 
+    if (u1.equals(u2)) {
+        System.out.println("Erro: Usuário já existe!");
+        System.out.println();
+        }else {
+        System.out.println("Cadastro liberado!");
+        System.out.println();
+    }  
 }
+
+}
+      
+
--- src/br/com/streaming/model/Usuario.java
+++ src/br/com/streaming/model/Usuario.java
@@ -9,6 +9,9 @@ public class Usuario {
     private String email;
     private String nome;
     private boolean ativo;
+    // segundo 3° Desafio o slide 35 aula 2.
+    public static final String NOME_PLATAFORMA = "JavaFlix"; 
+
     // TODO: Associação 1..* - Criar Lista de Videos (historicoAssistidos)
 
     public Usuario(String email, String nome) {
@@ -22,4 +25,69 @@ public class Usuario {
         // TODO: Adicionar o vídeo na lista de histórico
     }
     // TODO: Gerar Getters, Setters, equals(), hashCode() e toString()
+
+   public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+            "nome='" + nome + '\'' +
+            ", email='" + email + '\'' +
+            '}';
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + (ativo ? 1231 : 1237);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (ativo != other.ativo)
+            return false;
+        return true;
+    }
+    
+    public static int contadorUsuarios = 0;
+    public Usuario(){
+        contadorUsuarios++;
+    }
+
 }
